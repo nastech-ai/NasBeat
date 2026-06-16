fb10610ca532ce9838cc7913b6f3b1deccc05311
nastech-ai <bantuinvasions@gmail.com>
2026-06-16 08:30:21 +0200

    brand: package:Bloomee/ → package:nasbeat/ [more_opts_sheet.dart]

--- lib/screens/screen/library_views/more_opts_sheet.dart
+++ lib/screens/screen/library_views/more_opts_sheet.dart
@@ -8,15 +8,15 @@ import 'package:path_provider/path_provider.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:file_picker/file_picker.dart';
 
-import 'package:Bloomee/blocs/library/cubit/library_items_cubit.dart';
-import 'package:Bloomee/blocs/media_player/bloomee_player_cubit.dart';
-import 'package:Bloomee/core/models/media_playlist_model.dart';
-import 'package:Bloomee/screens/screen/library_views/cubit/current_playlist_cubit.dart';
-import 'package:Bloomee/screens/screen/library_views/playlist_edit_view.dart';
-import 'package:Bloomee/screens/widgets/snackbar.dart';
-import 'package:Bloomee/core/theme/app_theme.dart';
-import 'package:Bloomee/l10n/app_localizations.dart';
-import 'package:Bloomee/services/import_export_service.dart';
+import 'package:nasbeat/blocs/library/cubit/library_items_cubit.dart';
+import 'package:nasbeat/blocs/media_player/bloomee_player_cubit.dart';
+import 'package:nasbeat/core/models/media_playlist_model.dart';
+import 'package:nasbeat/screens/screen/library_views/cubit/current_playlist_cubit.dart';
+import 'package:nasbeat/screens/screen/library_views/playlist_edit_view.dart';
+import 'package:nasbeat/screens/widgets/snackbar.dart';
+import 'package:nasbeat/core/theme/app_theme.dart';
+import 'package:nasbeat/l10n/app_localizations.dart';
+import 'package:nasbeat/services/import_export_service.dart';
 
 /// Shows options for an internal/local playlist
 void showPlaylistOptsInrSheet(BuildContext context, Playlist playlist) {
